aec3ec5565e31857a944800928f40fee84d95585
Sindor Olmasov <sindordev77@gmail.com>
2025-05-23 18:40:03 +0500

    fixed bug on service folder

--- src/routes/home/Home.tsx
+++ src/routes/home/Home.tsx
@@ -1,7 +1,7 @@
 import Footer from "../../components/footer/Footer";
 import Hero from "../../components/hero/Hero";
 import Markque from "../../components/markque/Markque";
-import Services from "../../components/services/Services";
+import Services from "../../components/servic/Services";
 import Showcase from "../../components/showCase/Showcase";
 import Team from "../../components/team/Team";
 
